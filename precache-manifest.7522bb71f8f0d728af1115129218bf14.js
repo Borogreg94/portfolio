7de2ead5034ea2ae50ea6e25d566cde14b57f8af
Greg Borodulin <gregborodulin@Gregs-MBP-3.ptd.net>
2020-12-04 21:03:42 -0500

    Updates

---
+++ precache-manifest.7522bb71f8f0d728af1115129218bf14.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67a9cf2040098874ba0a04bb550e31a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "444da4869d5fad62e01e",
+    "url": "/portfolio/static/css/main.0dd3098c.chunk.css"
+  },
+  {
+    "revision": "f6d510ed38ffe121b82f",
+    "url": "/portfolio/static/js/2.fbded9b2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.fbded9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444da4869d5fad62e01e",
+    "url": "/portfolio/static/js/main.936339ed.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ac1839b05d92a3c9818f9063ad10098e",
+    "url": "/portfolio/static/media/45-degree-fabric-light2.ac1839b0.png"
+  },
+  {
+    "revision": "1e77b9a1a63d75e6d7df907d7045db37",
+    "url": "/portfolio/static/media/GregBorodulin_Resume.1e77b9a1.pdf"
+  },
+  {
+    "revision": "313f9d5daab37d19433df5e9faf7145f",
+    "url": "/portfolio/static/media/GregBorodulin_Resume.313f9d5d.png"
+  },
+  {
+    "revision": "a70efd80171bbf35c0555c123d86865e",
+    "url": "/portfolio/static/media/JosefinSansRegular.a70efd80.ttf"
+  },
+  {
+    "revision": "9a480bdd4e590cdb36c8eead284cff9c",
+    "url": "/portfolio/static/media/MunichRegular-Y8x4.9a480bdd.ttf"
+  },
+  {
+    "revision": "63bb2f3fa441c0366350160e6476d3ac",
+    "url": "/portfolio/static/media/alphabetizedcassettetapes.63bb2f3f.ttf"
+  },
+  {
+    "revision": "bc95bc6876a6e8bca9161ab82bf64176",
+    "url": "/portfolio/static/media/buried.bc95bc68.png"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/portfolio/static/media/css.6971ddc7.svg"
+  },
+  {
+    "revision": "1e966d2d744c814b841bf8e3020e9bdd",
+    "url": "/portfolio/static/media/durak_cover1.1e966d2d.PNG"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/portfolio/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/portfolio/static/media/html.f0361c5e.svg"
+  },
+  {
+    "revision": "d9e36452d84b2d19bfd2892ed2aca0bd",
+    "url": "/portfolio/static/media/igf_cover1.d9e36452.PNG"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "6c8e140e0c8b5d6acd513c67d12d267d",
+    "url": "/portfolio/static/media/linkedin.6c8e140e.svg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/portfolio/static/media/react.c3fbd71b.svg"
+  },
+  {
+    "revision": "b3e5602fcdde5e06c5e071803e871b09",
+    "url": "/portfolio/static/media/redux.b3e5602f.svg"
+  },
+  {
+    "revision": "5f33d4adf85d0d799516d1efcf03da0e",
+    "url": "/portfolio/static/media/tree.5f33d4ad.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d01d0bf8b75cba8e2e3a2a1cc852b08a.js"
+  "/portfolio/precache-manifest.7522bb71f8f0d728af1115129218bf14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
